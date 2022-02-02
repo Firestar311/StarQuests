000e754d389706be2fa8e43d20157daf4e6f11ab
Firestar311 <slaterandrew0398@gmail.com>
2022-02-02 03:23:11 -0600

    Basic Setup and/or update .gitignore

---
+++ src/main/java/com/starmediadev/plugins/starquests/StarQuests.java
@@ -0,0 +1,10 @@
+package com.starmediadev.plugins.starquests;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class StarQuests extends JavaPlugin {
+
+    public void onEnable() {
+        
+    }
+}
